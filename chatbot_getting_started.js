d4ad49fae23fb9f15ddb71d7ff8cfc128985c969
markeezy <markkhoo95@gmail.com>
2021-07-30 09:12:09 -0700

    testing

--- chatbot_getting_started.js
+++ chatbot_getting_started.js
@@ -1,4 +1,4 @@
-const tmi = require('tmi.js');
+const tmi = require('tmi.js'); // See https://tmijs.com/ for tmi docs
 require('dotenv').config();
 
 // Define configuration options
@@ -23,29 +23,33 @@ client.on('connected', onConnectedHandler);
 client.connect();
 
 // Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
+function onMessageHandler(target, context, msg, self) {
   if (self) { return; } // Ignore messages from the bot
 
   // Remove whitespace from chat message
   const commandName = msg.trim();
+
+  // console.log(msg);
 
   // If the command is known, let's execute it
   if (commandName === '!dice') {
     const num = rollDice();
     client.say(target, `You rolled a ${num}`);
     console.log(`* Executed ${commandName} command`);
+  } else if (commandName === `!console`) {
+    client.say(target, `Check your server console`);
   } else {
     console.log(`* Unknown command ${commandName}`);
   }
 };
 
 // Function called when the "dice" command is issued
-function rollDice () {
+function rollDice() {
   const sides = 6;
   return Math.floor(Math.random() * sides) + 1;
 };
 
 // Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
+function onConnectedHandler(addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
 };
